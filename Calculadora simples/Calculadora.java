68f8db42d46bb288a44063f14b984b61b05a5ef5
VictorGoya <victor.goya@edu.unifil.br>
2025-11-28 21:54:48 -0300

    Adicionado as funções Subtracao e Divisao

--- Calculadora simples/Calculadora.java
+++ Calculadora simples/Calculadora.java
@@ -2,4 +2,13 @@ public class Calculadora {
     public static void main(String[] args) {
 
     }
+
+    public int subtracao (int a, int b) {
+        return a+b;
+    }
+
+    public float divisao (int a, int b) {
+        return a/b;
+    }
+
 }
