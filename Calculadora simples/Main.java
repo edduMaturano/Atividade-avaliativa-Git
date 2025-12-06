d432e6010649b4a9dfc14eb8cda8e061f6246dc5
VictorGoya <victor.goya@edu.unifil.br>
2025-12-05 21:43:12 -0300

    Adicionada as funções adicao e multiplicacao junto com as chamadas switch-case para chamada do menu

--- Calculadora simples/Main.java
+++ Calculadora simples/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
@@ -8,7 +9,16 @@ public static void main(String[] args) {
         String opcao = input.nextLine();
         switch(opcao){
             case "1":
+                try {
+                    System.out.println("Digite a primeira parcela: ");
+                    float parcela1 = input.nextInt();
+                    System.out.println("Digite a segunda parcela: ");
+                    float parcela2 = input.nextInt();
+                    adicao(parcela1, parcela2, input);
+                } catch (InputMismatchException e) {
+                    System.out.println("Digite apenas números.");
 
+                }
                 break;
             case "2":
                 try {
@@ -23,8 +33,17 @@ public static void main(String[] args) {
                 }
                 break;
             case "3":
-                break;
+                try {
+                    System.out.println("Digite o multiplicando: ");
+                    float multiplicando = input.nextInt();
+                    System.out.println("Digite o multiplicador: ");
+                    float multiplicador = input.nextInt();
+                    multiplicacao(multiplicando, multiplicador, input);
+                } catch (InputMismatchException e) {
+                    System.out.println("Digite apenas números.");
 
+                }
+                break;
             case "4":
                 try {
                     System.out.println("Digite o dividendo");
@@ -89,6 +108,22 @@ static void divisao (float numero1, float numero2, Scanner input){
         input.nextLine();
         return;
     }
+    System.out.println("Aperte enter para voltar pro menu:");
+
+    input.nextLine();
+    input.nextLine();
+}
+
+static void adicao(float numero1, float numero2, Scanner input) {
+    System.out.println("O resultado da adicao foi " + (numero1 + numero2));
+    System.out.println("Aperte enter para voltar pro menu:");
+
+    input.nextLine();
+    input.nextLine();
+}
+
+static void multiplicacao(float numero1, float numero2, Scanner input) {
+    System.out.println("O resultado da multiplicação foi " + (numero1 * numero2));
     System.out.println("Aperte enter para voltar pro menu:");
 
     input.nextLine();
